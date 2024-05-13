c98426a238e3eb6279acf46ba958df178be0afe6
Douglas Nascimento <douglas_timaofiel@hotmail.com>
2024-05-13 17:58:22 -0300

    Initial commit

---
+++ src/test/java/com/douglas/springKafka/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.douglas.springKafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
