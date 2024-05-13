93d9d220f2c9b99a3a9a7206cf1ed8ffd3e0e1ed
Douglas Nascimento <douglas_timaofiel@hotmail.com>
2024-05-13 18:14:04 -0300

    Kafka support enabled

--- src/main/java/com/douglas/springKafka/Application.java
+++ src/main/java/com/douglas/springKafka/Application.java
@@ -2,10 +2,11 @@ package com.douglas.springKafka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
+@EnableKafka
 public class Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
