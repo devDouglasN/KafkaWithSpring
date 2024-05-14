b11c4837a4c08d906c408f1938441f1c45d3d902
Douglas Nascimento <douglas_timaofiel@hotmail.com>
2024-05-14 12:13:42 -0300

    Sending product information as JSON

--- src/main/java/com/douglas/springKafka/controller/ProducerController.java
+++ src/main/java/com/douglas/springKafka/controller/ProducerController.java
@@ -1,5 +1,8 @@
 package com.douglas.springKafka.controller;
 
+import com.douglas.springKafka.product.Product;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,8 +15,14 @@ public class ProducerController {
     @Autowired
     private KafkaTemplate<String, String> kafkaTemplate;
 
-    @GetMapping("/producer/{message}")
-    public void producer (@PathVariable String message) {
-        this.kafkaTemplate.send("ecomerce.cliente2", message);
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @GetMapping("/producer/{name}/{value}/{owner}")
+    public void producer (@PathVariable String name, @PathVariable Double value, @PathVariable String owner)
+            throws JsonProcessingException {
+        Product product = new Product(name, value, owner);
+
+        this.kafkaTemplate.send("ecomerce.cliente2", objectMapper.writeValueAsString(product));
     }
 }
